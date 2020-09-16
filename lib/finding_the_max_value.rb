e6f2692341844f0512afe1fb6fdc9a034b4f05b2
Kevin Grow <kevingrow11@gmail.com>
2020-09-16 20:40:32 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -1,10 +1,9 @@
 def find_max_value(array)
   count = 0
-  king_of_the_hill = [0]
+  king_of_the_hill = -1
   while count < array.length do 
-    if array[count] > king_of_the_hill[0] 
-      king_of_the_hill.delete_at(0)
-      king_of_the_hill.push(array[count])
+    if array[count] > king_of_the_hill
+      king_of_the_hill = array[count]
     end
     count += 1
   end
